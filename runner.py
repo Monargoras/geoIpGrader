0942fecc9a06fe2c1c0ae3aad72e12ca2f1edc6d
Felix Wagner <felixwagner99@googlemail.com>
2022-08-19 21:52:49 +0200

    remove main file, cleanup runner, testing

--- runner.py
+++ runner.py
@@ -1,7 +1,29 @@
 import grader
+import logging
 from Databases import Databases
+from DatabaseIO import DatabaseIO
+
+
+logging.getLogger().setLevel(logging.INFO)
+dbio = DatabaseIO()
+
+
+def printEvaluation():
+    res = {}
+    for d in [e.name for e in Databases]:
+        consistent, total = dbio.getEvaluation(d)
+        res[d] = [consistent, total, round((consistent / total), 4)]
+    x = 0
+    for d in [e.name for e in Databases]:
+        x += (1/(1-res[d][2])) if (1-res[d][2]) > 0 else 10000
+    for d in [e.name for e in Databases]:
+        # voting shares ausrechnen vs_i = (1/(1-DCR_i))/sum of for all dbs (1/(1-DCR_i))
+        x_d = (1/(1-res[d][2])) if (1-res[d][2]) > 0 else 10000
+        print(d, '-----', res[d][0], '-----', res[d][1], '-----', 'DCR:', str(round(res[d][2] * 100, 2)) + '%',
+              '-----', 'Voting share:', str(round((x_d/x) * 100, 2)) + '%')
 
 
 if __name__ == '__main__':
-    for d in [e.name for e in Databases]:
-        grader.gradeGeoIp(d)
+    for db in [e.name for e in Databases]:
+        grader.gradeGeoIp(db)
+    printEvaluation()
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from Databases import Databases
+import math
 from DatabaseIO import DatabaseIO
 import os
 import sys
+import utils
 
-print(sys.executable)
+ip = utils.ipToLong('104.44.29.68')
+apiResult = utils.ripeIpmapRequest(utils.longToIp(ip))
+print([ip, ip, apiResult[0], apiResult[1]])
+print(utils.ripeIpmapRequest('104.44.29.68'))
