0ae53e3a0c9d2505848931674c540bb98f746179
Felix Wagner <felixwagner99@googlemail.com>
2022-10-02 18:07:06 +0200

    add unique as data method

--- locationComparator.py
+++ locationComparator.py
@@ -56,6 +56,30 @@ def compareDCMPeering():
     print('DCM vs Peering', '-----', 'Equal:', totalEqualLocations, '-----', 'Unique', totalUniqueLocations)
 
 
+def countUniqueAS():
+    dbio = DatabaseIO()
+    dcmLoc = dbio.getDCMLocations()
+    index = 0
+    uniqueAsn = []
+    while index < len(dcmLoc):
+        curAsn = dcmLoc[index][1]
+        while index < len(dcmLoc) and dcmLoc[index][1] == curAsn:
+            index += 1
+        peeringAsn = dbio.getPeeringLocations(curAsn)
+        peeringCoords = [(lat, long) for (a, b, lat, long) in peeringAsn]
+        if not len(peeringCoords) > 0:
+            uniqueAsn.append(curAsn)
+    print('Unique ASNs', '-----', len(uniqueAsn))
+    total = 0
+    for asn in uniqueAsn:
+        dcmAsn = dbio.getDCMLocations(asn)
+        dcmCoords = [(lat, long) for (a, b, lat, long) in dcmAsn]
+        total += len(dcmCoords)
+    average = total / len(uniqueAsn)
+    print('Average locations per unique ASN', '-----', average)
+
+
 if __name__ == '__main__':
     comparePeeringDCM()
     compareDCMPeering()
+    countUniqueAS()
