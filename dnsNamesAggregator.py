03323c8db2155074f43bf7224f3f2f166fff8b6f
Felix Wagner <felixwagner99@googlemail.com>
2022-11-30 13:41:25 +0100

    split output to reduce memory usage

--- dnsNamesAggregator.py
+++ dnsNamesAggregator.py
@@ -1,5 +1,3 @@
-import re
-
 ipDict = {}
 day = 0
 
@@ -14,16 +12,26 @@ while day < 31:
             if len(splitLine) != 3:
                 invalidLines += 1
                 continue
-            splitDomain = re.split('[-.\\d]', splitLine[2])
-            if len(splitDomain) < 3 or splitDomain[0] == 'www':
+            domainFragments = splitLine[2].split('.')
+            if len(domainFragments) < 3 or domainFragments[0] == 'www':
                 invalidLines += 1
                 continue
             validLines += 1
             ipDict[splitLine[1]] = splitLine[2]
-        with open("data/log.txt", "a") as outputFile:
-            outputFile.write(filename + ' - ' + str(invalidLines) + ' - ' + str(validLines) + '\n')
+#        with open("data/log.txt", "a") as outputFile:
+#            outputFile.write(filename + ' - ' + str(invalidLines) + ' - ' + str(validLines) + '\n')
         print("Done with", filename)
 
-with open("data/aggregatedDnsNames.txt", "w") as outputFile:
-    for ip in ipDict:
-        outputFile.write(ip + ' ' + ipDict[ip] + '\n')
+fileNr = 0
+index = 0
+sortedDict = sorted(ipDict)
+while index < len(sortedDict):
+    fileNr += 1
+    if fileNr > 15:
+        break
+    with open("./data/aggregated/aggregatedDnsNames{fileNr}.txt".format(fileNr=fileNr), "w") as outputFile:
+        while True:
+            outputFile.write(sortedDict[index] + ' ' + ipDict[sortedDict[index]] + '\n')
+            index += 1
+            if index % 6000000 == 0 or index >= len(sortedDict):
+                break
