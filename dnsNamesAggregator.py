ce5341039563a29f434854217c1838ad264e2c38
Felix Wagner <felixwagner99@googlemail.com>
2022-11-30 14:36:25 +0100

    add log aggregator

--- dnsNamesAggregator.py
+++ dnsNamesAggregator.py
@@ -1,37 +1,53 @@
-ipDict = {}
-day = 0
+def aggregateDnsNames():
+    ipDict = {}
+    day = 0
 
-while day < 31:
-    day += 1
-    filename = "dns-names.l7.202110{day}.txt".format(day=str(day).zfill(2))
-    with open("./data/dns_data/" + filename, "r") as inputFile:
-        invalidLines = 0
-        validLines = 0
+    while day < 31:
+        day += 1
+        filename = "dns-names.l7.202110{day}.txt".format(day=str(day).zfill(2))
+        with open("./data/dns_data/" + filename, "r") as inputFile:
+            invalidLines = 0
+            validLines = 0
+            for line in inputFile:
+                splitLine = line.split()
+                if len(splitLine) != 3:
+                    invalidLines += 1
+                    continue
+                domainFragments = splitLine[2].split('.')
+                if len(domainFragments) < 3 or domainFragments[0] == 'www':
+                    invalidLines += 1
+                    continue
+                validLines += 1
+                ipDict[splitLine[1]] = splitLine[2]
+    #        with open("data/log.txt", "a") as outputFile:
+    #            outputFile.write(filename + ' - ' + str(invalidLines) + ' - ' + str(validLines) + '\n')
+            print("Done with", filename)
+
+    fileNr = 0
+    index = 0
+    sortedDict = sorted(ipDict)
+    while index < len(sortedDict):
+        fileNr += 1
+        if fileNr > 15:
+            break
+        with open("./data/aggregated/aggregatedDnsNames{fileNr}.txt".format(fileNr=fileNr), "w") as outputFile:
+            while True:
+                outputFile.write(sortedDict[index] + ' ' + ipDict[sortedDict[index]] + '\n')
+                index += 1
+                if index % 6000000 == 0 or index >= len(sortedDict):
+                    break
+
+
+def aggregateLogs():
+    totalInvalid = 0
+    totalValid = 0
+    with open("./data/log.txt", "r") as inputFile:
         for line in inputFile:
             splitLine = line.split()
-            if len(splitLine) != 3:
-                invalidLines += 1
-                continue
-            domainFragments = splitLine[2].split('.')
-            if len(domainFragments) < 3 or domainFragments[0] == 'www':
-                invalidLines += 1
-                continue
-            validLines += 1
-            ipDict[splitLine[1]] = splitLine[2]
-#        with open("data/log.txt", "a") as outputFile:
-#            outputFile.write(filename + ' - ' + str(invalidLines) + ' - ' + str(validLines) + '\n')
-        print("Done with", filename)
+            totalInvalid += int(splitLine[2])
+            totalValid += int(splitLine[4])
+    print(totalInvalid, totalValid, totalInvalid + totalValid)
 
-fileNr = 0
-index = 0
-sortedDict = sorted(ipDict)
-while index < len(sortedDict):
-    fileNr += 1
-    if fileNr > 15:
-        break
-    with open("./data/aggregated/aggregatedDnsNames{fileNr}.txt".format(fileNr=fileNr), "w") as outputFile:
-        while True:
-            outputFile.write(sortedDict[index] + ' ' + ipDict[sortedDict[index]] + '\n')
-            index += 1
-            if index % 6000000 == 0 or index >= len(sortedDict):
-                break
+
+if __name__ == '__main__':
+    aggregateLogs()
